715507a319c0ed85e90bdd813db155b422773568
anumberplate <aluma.kyle@gmail.com>
2025-08-07 15:23:58 +0300

    fixed imports

--- myapp/src/Components/Singlepost.jsx
+++ myapp/src/Components/Singlepost.jsx
@@ -2,11 +2,7 @@ import { useParams } from 'react-router-dom';
 import React, { useState, useEffect } from "react"
 import axios from 'axios'
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
-import {
-  faHeart as faHeartRegular,
-  faComment as faCommentRegular,
-  faShare
-} from '@fortawesome/free-regular-svg-icons';
+import { faHeart as faHeartRegular,faComment as faCommentRegular} from '@fortawesome/free-regular-svg-icons';
 import { faHeart as faHeartSolid } from '@fortawesome/free-solid-svg-icons';
 
 export default function SinglePost() {
@@ -97,9 +93,6 @@ export default function SinglePost() {
             </button>
             <button className="md:text-2xl">
               <FontAwesomeIcon icon={faCommentRegular} className="cursor-pointer hover:text-blue-500" />
-            </button>
-            <button className="md:text-2xl">
-              <FontAwesomeIcon icon={faShare} className="cursor-pointer hover:text-green-500" />
             </button>
           </div>
         </div>
