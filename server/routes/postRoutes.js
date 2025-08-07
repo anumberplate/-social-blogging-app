b9097a436ba16666e0ab9587008617c70bf791cf
anumberplate <aluma.kyle@gmail.com>
2025-08-07 11:49:27 +0300

    fixed backend

--- server/routes/postRoutes.js
+++ server/routes/postRoutes.js
@@ -10,7 +10,7 @@ router.post('/', auth, postController.createPost);             // Create post
 router.put('/:id', auth, postController.updatePost);           // Update post 
 router.delete('/:id', auth, postController.deletePost);        // Delete post 
 router.get('/:id', postController.getPostById);                // Get single post 
-router.patch('/:id/like', auth, postController.toggleLike);    // Toggle like
+router.patch('/:id/like', auth, postController.toggleLikePost);  // Toggle like
 router.post('/:id/comments', auth, postController.addComment); // Add comment 
 
 module.exports = router;
